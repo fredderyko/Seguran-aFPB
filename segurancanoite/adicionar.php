13a5ddcf201f79ac2e6a65fd1eb955a917fc1ba9
Fredderyko <106125203+fredderyko@users.noreply.github.com>
2023-10-31 21:13:10 -0300

    Add files via upload

---
+++ segurancanoite/adicionar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        
+        <form id="form add"
+        action= "#" method="POST"
+        nome: <input type= "text" nome="nome"><br>
+        cpf: <input type= "text" nome="cpf"><br>
+        telefone: <input type= "text" nome="telefone"><br>
+        login: <input type= "text" nome="login"><br>
+        senha: <input type= "password" nome="senha"><br>
+        <input type="submit" nome="cadastrar" value="cadastrar">
+
+
+</center>
+</body>
+</html>
